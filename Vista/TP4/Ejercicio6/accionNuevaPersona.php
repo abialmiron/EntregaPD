24539c3b971d6c987d3ea3179df4121aae7a2bfe
agusflores1 <agustinaafff@gmail.com>
2023-09-15 15:27:53 -0300

    ej6 listo

--- Vista/TP4/Ejercicio6/accionNuevaPersona.php
+++ Vista/TP4/Ejercicio6/accionNuevaPersona.php
@@ -1,12 +1,11 @@
 
 <?php
-    include_once ("../Estructura/cabecera.php");
-    include_once ('../../Control/ControlAuto.php');
-    include_once ('../../Control/ControlPersona.php');
-    include_once '../../configuracion.php';
-  //  include_once("../../util/funciones.php");
-    ?>
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mb-4" style="margin-top: 10px;">
+include_once("../../../includes/configuracion.php");
+include_once(STRUCTURE_PATH . "head.php");
+include_once(ROOT_PATH.'/Control/TP4/ControlAuto.php');
+include_once(ROOT_PATH.'/Control/TP4/ControlPersona.php');
+?>
+  <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mb-4" style="margin-top: 10px;">
     <div class="card w-50 mb-4">
         <div class="card-body">
             <h5 class="card-title">Resultado:</h5>
@@ -21,14 +20,11 @@
             }
             ?>
              <div class="d-flex justify-content-end align-items-end mt-4 pr-3 pb-3">
-            <a href="index.php" class="btn btn-primary">Volver</a>
+            <a href="NuevaPersona.php" class="btn btn-primary">Volver</a>
         </div>
         </div>
     </div>
 </main>
-    <?php
-      include ("../Estructura/pie.php");
-    ?>
 
-</body>
-</html>
+
+<?php include(STRUCTURE_PATH . "footer.php"); ?>
